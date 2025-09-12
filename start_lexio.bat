ffb9b552e92b781b36a0730b3435a31308b027d3
Tonyisstudying <manhcuongnguyen2726@gmail.com>
2025-09-12 18:09:17 +0800

    update batch file

--- start_lexio.bat
+++ start_lexio.bat
@@ -5,11 +5,21 @@ echo       Welcome to Lexio Language Learning
 echo ====================================================
 echo.
 
-REM Open the local HTML file directly
-start "" "index.html" 
+REM Get the directory where the batch file is located
+cd /d "%~dp0"
 
-echo Lexio has been launched!
-echo.
-echo If the application didn't open, please try manually opening index.html
+REM Check if index.html exists in the current directory
+if exist "index.html" (
+    REM Open the local HTML file directly
+    start "" "index.html"
+    echo Lexio has been launched!
+) else (
+    echo ERROR: index.html was not found in this directory.
+    echo Current directory: %CD%
+    echo.
+    echo Please make sure you extracted all files from the zip archive
+    echo and are running this batch file from the correct location.
+)
+
 echo.
 pause
